79e4284c57f0c36809c237892e70a6770acc2c01
darya <daryanovak18@gmail.com>
2019-09-18 22:30:48 +0300

    feat: add recipe module

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Apple Pie',
+      'cook 20 minutes',
+      'https://cdn3.tmbi.com/toh/GoogleImagesPostCard/exps6086_HB133235C07_19_4b_WEB.jpg'),
+      new Recipe(
+        'Apple Pie2',
+        'cook 30 minutes',
+        'https://img1.southernliving.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2017/10/main/arkansas_black_apple_pie_with_caramel_sauce_2540501_pieso_675.jpg?itok=e6AfBEXc'),
+
+  ];
 
   constructor() { }
 
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -3,9 +3,9 @@ export class Recipe {
   public description: string;
   public imagePath: string;
 
-  constructor(name: string, desc: string, imagePage: string){
+  constructor(name: string, desc: string, imagePath: string){
     this.name = name;
     this.description = desc;
-    this.imagePath = this.imagePath;
+    this.imagePath = imagePath;
   }
 }
